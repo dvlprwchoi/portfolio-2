94245831c28f821b41c848766fe352a10c5abf56
dvlprwchoi <dvlpr.wchoi@gmail.com>
2022-01-21 11:44:58 -0600

    Add Navbar style

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,28 @@
+import './Navbar.css';
+
 function Navbar() {
-  return <div className="navbar">nav</div>;
+  return (
+    <div className="navbar">
+      <ul>
+        <li>
+          <a href="#">HOME</a>
+        </li>
+        <li>
+          <a href="#">ABOUT</a>
+        </li>
+        <li>
+          <a href="#">PROJECTS</a>
+        </li>
+        <li>
+          <a href="#">RESUME</a>
+        </li>
+        <li>
+          <a href="#">CONTACT</a>
+        </li>
+        <button>theme</button>
+      </ul>
+    </div>
+  );
 }
 
 export default Navbar;
