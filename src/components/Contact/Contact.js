0d59b789746eca632bfcd25919503424d6ffeda7
dvlprwchoi <dvlpr.wchoi@gmail.com>
2022-01-22 21:02:01 -0600

    Add contact icons

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,6 +1,8 @@
 import './Contact.css';
-
-import React from 'react';
+import linkedinIcon from '../../img/linkedin-icon.png';
+import githubIcon from '../../img/github-icon.png';
+import gmailIcon from '../../img/gmail-icon.png';
+import twitterIcon from '../../img/twitter-icon.png';
 
 function Contact() {
   return (
@@ -8,27 +10,64 @@ function Contact() {
       <h2>CONTACT</h2>
       <div className="contact-information-container">
         <ul>
-          <li>
-            <a href="https://www.linkedin.com/in/woosikchoi" target="_blank">
-              Linked In
-            </a>
-          </li>
-          <li>
-            <a href="https://www.github.com/dvlprwchoi" target="_blank">
-              GitHub
-            </a>
-          </li>
-          <li>
+          <li className="icon-container">
             <a
+              className="icon-link-container"
+              href="https://www.linkedin.com/in/woosikchoi"
+              target="_blank"
+            >
+              <img
+                className="linkedin-icon icon"
+                src={linkedinIcon}
+                alt="LinkedIn Icon"
+                width="15%"
+                height="auto"
+              />
+            </a>
+          </li>
+          <li className="icon-container">
+            <a
+              className="icon-link-container"
+              href="https://www.github.com/dvlprwchoi"
+              target="_blank"
+            >
+              <img
+                className="github-icon icon"
+                src={githubIcon}
+                alt="GitHub Icon"
+                width="15%"
+                height="auto"
+              />
+            </a>
+          </li>
+          <li className="icon-container">
+            <a
+              className="icon-link-container"
               href="mailto:dvlpr.wchoi@gmail.com?subject=Mail from woosikchoi.com"
               target="_blank"
             >
-              email
+              <img
+                className="gmail-icon icon"
+                src={gmailIcon}
+                alt="Gmail Icon"
+                width="15%"
+                height="auto"
+              />
             </a>
           </li>
-          <li>
-            <a href="https://www.twitter.com/dvlprwchoi" target="_blank">
-              Twitter
+          <li className="icon-container">
+            <a
+              className="icon-link-container"
+              href="https://www.twitter.com/dvlprwchoi"
+              target="_blank"
+            >
+              <img
+                className="twitter-icon icon"
+                src={twitterIcon}
+                alt="Twitter Icon"
+                width="15%"
+                height="auto"
+              />
             </a>
           </li>
         </ul>
