86c931b64708d7c33d3cc78611080f7fd6acbdf9
dvlprwchoi <dvlpr.wchoi@gmail.com>
2022-05-18 09:39:33 -0500

    Update resume

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -55,7 +55,7 @@ function Main() {
             <li className="resume-icon-container">
               <a
                 className="icon-link-container"
-                href="https://drive.google.com/file/d/1lYKApz4nQSY8Cx7r59xJzAAuWvFgvuXv/view?usp=sharing"
+                href="https://drive.google.com/file/d/1ZnQjOo3AIKAiChLfJc0APHGKbZMwteEa/view?usp=sharing"
                 target="_blank"
                 rel="noreferrer"
               >
